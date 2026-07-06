9c05f482044c439c60fec648a731dc24366e536b
Souvik Pal <souvikp2001@gmail.com>
2026-07-06 19:34:14 +0530

    Update product image handling

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -17,15 +17,17 @@ function ProductCard({ product }: ProductCardProps) {
     <Link to={`/product/${product.id}`}>
       <div className="bg-white rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] transition-transform p-4 cursor-pointer">
         <img
-          src= {product.image}
+          src={`/products/${product.image}`}
           alt={product.name}
           className="w-full h-56 object-cover rounded-lg mb-4"
         />
-        <h2 className="text-2xl font-bold text-gray-800 truncate">{product.name}</h2>
+        <h2 className="text-2xl font-bold text-gray-800 truncate">
+          {product.name}
+        </h2>
         <p className="text-2xl font-bold text-black-600">₹{product.price}</p>
-    </div>  
-    </Link>  
-    )
+      </div>
+    </Link>
+  );
   
 }
 
